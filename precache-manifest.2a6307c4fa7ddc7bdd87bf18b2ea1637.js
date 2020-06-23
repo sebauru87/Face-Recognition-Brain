70ccf5e5cae457ec9fef517498be8b9187bfb0bb
Sebastian <Ferreira>
2020-06-23 15:28:58 -0300

    Updates

---
+++ precache-manifest.2a6307c4fa7ddc7bdd87bf18b2ea1637.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd03af58b827381b29abdb62cb1b5e0b",
+    "url": "/Face-Recognition-Brain/index.html"
+  },
+  {
+    "revision": "84568b1d9f47333e2fd4",
+    "url": "/Face-Recognition-Brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4a90f902ce6192d14ea1",
+    "url": "/Face-Recognition-Brain/static/css/main.2e8adcd2.chunk.css"
+  },
+  {
+    "revision": "84568b1d9f47333e2fd4",
+    "url": "/Face-Recognition-Brain/static/js/2.7dcfd453.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Face-Recognition-Brain/static/js/2.7dcfd453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a90f902ce6192d14ea1",
+    "url": "/Face-Recognition-Brain/static/js/main.d1eba46a.chunk.js"
+  },
+  {
+    "revision": "248500559b2499c18046",
+    "url": "/Face-Recognition-Brain/static/js/runtime-main.e661ae86.js"
+  }
+]);
