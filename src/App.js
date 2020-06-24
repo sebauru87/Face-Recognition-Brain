1f75e7d7ac28c19ce2819c291516935e0530890c
Sebastian <Ferreira>
2020-06-24 14:10:31 -0300

    add facerecognition css. In app.js add functionality to calculate face in a box

--- src/App.js
+++ src/App.js
@@ -57,8 +57,26 @@ class App extends Component {
     super();
     this.state = {
       input: "",
-      imageUrl: ""
+      imageUrl: "",
+      box: {}
     };
+  }
+  calculateFaceLocation = (data) =>{
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    //console.log(width,height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width -  (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
   }
   onInputChange = (event) => {
     this.setState({input: event.target.value})
@@ -70,16 +88,11 @@ class App extends Component {
       .predict(
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
-      .then(
-        function (response) {
-          // do something with response
-          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-        },
-        function (err) {
-          // there was an error
-        }
-      );
-  };
+      .then((response)=> {
+          this.displayFaceBox(this.calculateFaceLocation(response));
+        })
+        .catch(error=> console.log(error));
+  }
   render() {
     return (
       <div className="App">
@@ -91,7 +104,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box}/>
       </div>
     );
   }
