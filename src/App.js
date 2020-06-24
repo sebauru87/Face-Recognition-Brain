729a725d448f7e3cfcae161827b6e77bd860f299
Sebastian <Ferreira>
2020-06-23 22:44:22 -0300

    add on input change and on button submit with state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
-import Particles from 'react-particles-js';
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
+import React, { Component } from "react";
+import "./App.css";
+import Particles from "react-particles-js";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
 
 const particlesOptions = {
   particles: {
@@ -12,22 +12,22 @@ const particlesOptions = {
       value: 80,
       density: {
         enable: true,
-        value_area: 500
-      }
-    }
+        value_area: 500,
+      },
+    },
   },
   interactivity: {
     detectsOn: "canvas",
     events: {
       onClick: {
         enable: true,
-        mode: "push"
+        mode: "push",
       },
       onHover: {
         enable: true,
-        mode: "repulse"
+        mode: "repulse",
       },
-      resize: true
+      resize: true,
     },
     modes: {
       bubble: {
@@ -35,30 +35,44 @@ const particlesOptions = {
         duration: 2,
         opacity: 0.8,
         size: 40,
-        speed: 3
+        speed: 3,
       },
       push: {
-        quantity: 4
+        quantity: 4,
       },
       repulse: {
         distance: 200,
-        duration: 0.4
-      }
-    }
-  }
-}
+        duration: 0.4,
+      },
+    },
+  },
+};
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles' params={particlesOptions} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+  onButtonSubmit = ()=>{
+    console.log('click');
+  }
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
