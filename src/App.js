71955722c94015db27d87e122e620d68fa5a7d3b
Sebastian <Ferreira>
2020-06-25 00:02:15 -0300

    Add Register, and if else statements for register, sign in, sign out

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Particles from "react-particles-js";
 import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
@@ -60,7 +61,8 @@ class App extends Component {
       input: "",
       imageUrl: "",
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     };
   }
   calculateFaceLocation = (data) => {
@@ -96,6 +98,11 @@ class App extends Component {
   };
 
   onRouteChange = (route) => {
+    if(route === 'signout'){
+      this.setState({isSignedIn: false});
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true});
+    }
     this.setState({route: route});
   }
 
@@ -103,10 +110,9 @@ class App extends Component {
     return (
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
-          <Navigation onRouteChange={this.onRouteChange}/>
-          { this.state.route === 'signin'
-          ? <Signin onRouteChange={this.onRouteChange} />
-          : <div>
+          <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
+          { this.state.route === 'home'
+          ? <div>
           <Logo />
           <Rank />
           <ImageLinkForm
@@ -115,6 +121,11 @@ class App extends Component {
           />
           <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box} />
           </div>
+          : (
+            this.state.route === 'signin'
+            ? <Signin onRouteChange={this.onRouteChange} />
+            : <Register onRouteChange={this.onRouteChange} />
+          )
         }
       </div>
     );
